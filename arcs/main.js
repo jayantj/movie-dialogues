29804cab0e495180fa5177830dd9cd250f8d417d
Kelly Su <yoyo420303@gmail.com>
2018-11-27 12:46:25 -0500

    multiple arcs of male

--- arcs/main.js
+++ arcs/main.js
@@ -6,7 +6,9 @@ var svg = d3.select('svg');
 var svgWidth = +svg.attr('width');
 var svgHeight = +svg.attr('height');
 
-var padding = {t: 40, r: 40, b: 40, l: 40};
+var padding = {t: 40, r: 90, b: 40, l: 90};
+
+var rectNodeSize = 30;
 
 // Compute chart dimensions
 var chartWidth = svgWidth - padding.l - padding.r;
@@ -88,7 +90,7 @@ d3.json('./m0.json', function(error, dataset) {
       d.target = allCharacters.find(el => el.id === d.target);
     });
 
-    drawLinks(maleG, links, mRadianScale);
+    drawLinks(maleG, links, mRadianScale, malexfix);
     drawCrossLinks(cLinks, maleScale, femaleScale);
     drawNodes(maleG, male, malexfix, maleScale);
     drawNodes(femaleG, female, femaleyfix, femaleScale);
@@ -100,8 +102,8 @@ function linearLayout(nodes, fix, scale) {
       d.y = scale(i);
   })
 }
+
 function drawCrossLinks (links, s1, s2) {
-  console.log("cross", links);
   svg.selectAll('line')
   .data(links)
   .enter()
@@ -118,19 +120,48 @@ function drawCrossLinks (links, s1, s2) {
   .style('stroke', '#000');
 }
 
-function drawLinks(group, links, scale) {
+function drawLinks(group, links, scale, xFix) {
   var arc = d3.lineRadial()
   // .interpolate()
   // .tension(0)
   .angle(function(d) { return scale(d); });
 
+  const linksOfConv = links.flatMap((l, i) => {
+    const conversations = Array.from({length: l.lines.length}, (c, j) => {
+      let {y, ...source} = l.source;
+      const temp = {
+        source: {
+          cname: l.source.cname,
+          gender: l.source.gender,
+          id: l.source.id,
+          movieId: l.source.movieId,
+          movieTitle: l.source.movieTitle,
+          x: l.source.x,
+          y: (rectNodeSize / l.counts) * j + l.source.y,
+        },
+        target: {
+          cname: l.target.cname,
+          gender: l.target.gender,
+          id: l.target.id,
+          movieId: l.target.movieId,
+          movieTitle: l.target.movieTitle,
+          x: l.target.x,
+          y: (rectNodeSize / l.counts) * j + l.target.y,
+        },
+        movieId: l.movieId,
+      };
+      return temp;
+    });
+    return conversations;
+  });
+
   group.selectAll('path')
-  .data(links)
+  .data(linksOfConv)
   .enter()
   .append('path')
   .attr('class', 'link')
   .attr('transform', function(d, i) {
-    return `translate(140,${d.source.y + (d.target.y-d.source.y)/2}) rotate(90)`
+    return `translate(${xFix},${d.source.y + (d.target.y-d.source.y)/2}) rotate(90)`
   })
   .attr('d', function(d, i) {
       var xDist = Math.abs(d.target.y - d.source.y);
@@ -147,13 +178,15 @@ function drawNodes(group, nodes, fix, scale) {
   group.selectAll('circle')
   .data(nodes)
   .enter()
-  .append('circle')
+  .append('rect')
   .attr('class', 'node')
-  .attr('cx', function(d, i) {
+  .attr('x', function(d, i) {
       return fix;
   })
-  .attr('cy', function(d, i) {
+  .attr('y', function(d, i) {
       return scale(i);
   })
-  .attr('r', radius);
+  .attr('width', '5px')
+  .attr('height', `${rectNodeSize}px`)
+  .attr('fill', 'red');
 };
