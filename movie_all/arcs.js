67e0aa9f99d5c45df4d47818b15b8d718cadb8a9
Eyitemi Moju-Igbene <moju@gatech.edu>
2018-12-01 23:12:20 -0500

    combine all the views

---
+++ movie_all/arcs.js
@@ -0,0 +1,239 @@
+// Global functions called when select elements changed
+(function() {
+var svgMain = d3.select('#main').select('svg');
+
+// Get layout parameters
+var svgMainWidth = +svgMain.attr('width');
+var svgMainHeight = +svgMain.attr('height');
+
+var MainPadding = {t: 40, r: 180, b: 40, l: 180};
+
+var rectNodeHeight = 60;
+var rectNodeWidth = 5;
+
+// Compute chart dimensions
+var mainChartWidth = svgMainWidth - MainPadding.l - MainPadding.r;
+var mainChartHeight = svgMainHeight - MainPadding.t - MainPadding.b;
+console.log('width and height: ', mainChartHeight);
+
+// fixed node radius
+var radius = 4;
+var malexfix = MainPadding.l + 100;
+var femalefix = svgMainWidth - MainPadding.r - 100;
+
+// Create a group element for appending chart elements
+var chartG = svgMain.append('g')
+    .attr('transform', 'translate('+[MainPadding.l, MainPadding.t]+')');
+
+// Create groups for the x- and y-axes
+var xAxisG = chartG.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate('+[0, mainChartHeight]+')');
+var yAxisG = chartG.append('g')
+    .attr('class', 'y axis');
+
+var cars = [];
+var maleScale = function() { return ; };
+var femaleScale = function() { return ; };
+var male = [];
+var female = [];
+
+var mRadianScale = function() { return ;};
+var fRadianScale = function() { return ;};
+
+
+d3.json('./../data/conv/m0.json', function(error, dataset) {
+    // Log and return from an error
+    console.log(dataset);
+    if(error) {
+        console.error('Error while loading ./m0.json dataset.');
+        console.error(error);
+        return;
+    }
+    // Create global object called chartScales to keep state
+    chartScales = {x: 'Male', y: 'Female'};
+
+    male = dataset.male;
+    female = dataset.female;
+    maleLinks = dataset.maleLinks;
+    femaleLinks = dataset.femaleLinks;
+    cLinks = dataset.crossGenderLinks;
+    maleScale = d3.scaleLinear()
+    .domain([0, male.length - 1])
+    .range([MainPadding.t, mainChartHeight - MainPadding.b]);
+    femaleScale = d3.scaleLinear()
+    .domain([0, female.length - 1])
+    .range([MainPadding.t, mainChartHeight - MainPadding.b]);
+
+    mRadianScale = d3.scaleLinear()
+    .range([Math.PI / 2, 3 * Math.PI / 2]);
+
+    fRadianScale = d3.scaleLinear()
+    .range([3 * Math.PI / 2, 5 * Math.PI / 2]);
+
+    // dataset.columns.forEach(function(column) {
+    //     domainMap[column] = d3.extent(dataset, function(data_element){
+    //         return data_element[column];
+    //     });
+    // });
+    const maleG = svgMain.append('g').attr('class', 'male');
+    const femaleG = svgMain.append('g').attr('class', 'female');
+    const crossG = svgMain.append('g').attr('class', 'cross-g');
+    linearLayout(male, malexfix, maleScale);
+    linearLayout(female, femalefix, femaleScale);
+
+    const allCharacters = male.concat(female);
+    
+    maleLinks.forEach(function(d, i) {
+      // d.source = isNaN(d.source) ? d.source : male[d.source-1];
+      d.source = allCharacters.find(el => el.id === d.source);
+      // d.target = isNaN(d.target) ? d.target : male[d.target-1];
+      d.target = allCharacters.find(el => el.id === d.target);
+    });
+
+    femaleLinks.forEach(function(d, i) {
+      d.source = allCharacters.find(el => el.id === d.source);
+      d.target = allCharacters.find(el => el.id === d.target);
+    });
+
+    cLinks.forEach((d, i) => {
+      d.source = allCharacters.find(el => el.id === d.source);
+      d.target = allCharacters.find(el => el.id === d.target);
+    });
+
+    drawLinks(maleG, maleLinks, mRadianScale, malexfix);
+    drawLinks(femaleG, femaleLinks, fRadianScale, femalefix);
+    drawCrossLinks(crossG, cLinks, maleScale, femaleScale);
+    drawNodes(maleG, male, malexfix, maleScale);
+    drawNodes(femaleG, female, femalefix, femaleScale);
+});
+
+function linearLayout(nodes, fix, scale) {
+  nodes.forEach(function(d, i) {
+      d.x = fix;
+      d.y = scale(i);
+  })
+}
+
+function drawCrossLinks (group, links, s1, s2) {
+  const linksOfConv = links.flatMap((l, i) => {
+    const conversations = Array.from({length: l.lines.length}, (c, j) => {
+      const interval = (l.counts > 1) ? (l.counts - 1) : l.counts;
+      const temp = {
+        source: {
+          cname: l.source.cname,
+          gender: l.source.gender,
+          id: l.source.id,
+          movieId: l.source.movieId,
+          movieTitle: l.source.movieTitle,
+          x: l.source.x + rectNodeWidth,
+          y: (rectNodeHeight / interval) * j + l.source.y,
+          conv: l.lines[j],
+        },
+        target: {
+          cname: l.target.cname,
+          gender: l.target.gender,
+          id: l.target.id,
+          movieId: l.target.movieId,
+          movieTitle: l.target.movieTitle,
+          x: l.target.x,
+          y: (rectNodeHeight / interval) * j + l.target.y,
+          conv: l.lines[j],
+        },
+        movieId: l.movieId,
+      };
+      return temp;
+    });
+    return conversations;
+  });
+
+  group.selectAll('line')
+  .data(linksOfConv)
+  .enter()
+  .append('line')
+  .attr('class', 'cLink')
+  .attr('x1', function(d, i) {
+    return d.source.x;
+  })
+  .attr('y1', function(d, i) {
+    return d.source.y;
+  })
+  .attr('x2', function(d, i) {
+    return d.target.x;
+  })
+  .attr('y2', function(d, i) {
+    return d.target.y;
+  });
+  //.style('stroke', '#000');
+}
+
+function drawLinks(group, links, scale, xFix) {
+  var arc = d3.lineRadial()
+  // .interpolate()
+  // .tension(0)
+  .angle(function(d) { return scale(d); });
+
+  const linksOfConv = links.flatMap((l, i) => {
+    const conversations = Array.from({length: l.lines.length}, (c, j) => {
+      let {y, ...source} = l.source;
+      const interval = (l.counts > 1) ? (l.counts - 1) : l.counts;
+      const temp = {
+        source: {
+          source,
+          y: (rectNodeHeight / interval) * j + l.source.y,
+          conv: l.lines[j],
+        },
+        target: {
+          cname: l.target.cname,
+          gender: l.target.gender,
+          id: l.target.id,
+          movieId: l.target.movieId,
+          movieTitle: l.target.movieTitle,
+          x: l.target.x,
+          y: (rectNodeHeight / interval) * j + l.target.y,
+          conv: l.lines[j],
+        },
+        movieId: l.movieId,
+      };
+      return temp;
+    });
+    return conversations;
+  });
+
+  group.selectAll('path')
+  .data(linksOfConv)
+  .enter()
+  .append('path')
+  .attr('class', 'link')
+  .attr('transform', function(d, i) {
+    return `translate(${xFix},${d.source.y + (d.target.y-d.source.y)/2}) rotate(90)`
+  })
+  .attr('d', function(d, i) {
+      var xDist = Math.abs(d.target.y - d.source.y);
+      arc.radius(xDist / 2);
+      var points = d3.range(0, Math.ceil(xDist / 3));
+      scale.domain([0, points.length - 1]);
+      return arc(points);
+  })
+  //.style('stroke', '#000')
+  .style('fill', 'none');
+}
+
+function drawNodes(group, nodes, fix, scale) {
+  group.selectAll('circle')
+  .data(nodes)
+  .enter()
+  .append('rect')
+  .attr('class', 'node')
+  .attr('x', function(d, i) {
+      return fix;
+  })
+  .attr('y', function(d, i) {
+      return scale(i);
+  })
+  .attr('width', `${rectNodeWidth}px`)
+  .attr('height', `${rectNodeHeight}px`)
+  .attr('fill', 'red');
+};
+
+})();
---
+++ movie_all/scatter.js
@@ -0,0 +1,247 @@
+var onGenreChanged;
+
+(function() {    
+    var dataDir = '../data/'
+    var svg = d3.select('svg');
+
+    // Hand code the svg dimensions, you can also use +svg.attr('width') or +svg.attr('height')
+    var svgWidth = +svg.attr('width');
+    var svgHeight = +svg.attr('height');
+    var padding = {t: 40, r: 40, b: 40, l: 40};
+    var chartpad = 10;
+
+    // Compute chart dimensions
+    var movies;
+
+    // Create a group element for appending chart elements
+    var chartG = svg.append('g')
+        .attr('transform', 'translate('+[padding.l, padding.t]+')');
+
+    var dataAttributes = ['mm_percent', 'mf_percent', 'ff_percent'];
+    var N = dataAttributes.length;
+    var chartWidth = (svgWidth - padding.l - padding.r);
+    var chartHeight = (svgHeight - padding.t - padding.b)/N;
+
+    //Global x and y scale and axies for 3 scatterplots
+    xScale = d3.scaleLinear().range([0, chartWidth - chartpad]);
+    yScale = d3.scaleLinear().range([chartHeight - chartpad, 0]);
+    var xAxis = d3.axisBottom(xScale).tickValues([1920,1930,1940,1950,1960,1970,1980,1990,2000]);//.tickFormat(d3.format(".0%"));
+    var yAxis = d3.axisLeft(yScale);
+
+    //ordinal color scale
+    var colorScale = d3.scaleOrdinal(d3.schemeSet2).domain(['1','0','?']);
+
+    // Map for referencing min/max per each attribute
+    var extentByAttribute = {};
+    var cellEnter;
+    var genre;
+
+    //Create Splom Cell
+    function SplomCell(x, y, col, row) {
+        this.x = x;
+        this.y = y;
+        this.col = col;
+        this.row = row;
+    }
+
+
+    //Create Cells to structure scatterplots
+    var cells = [];
+    dataAttributes.forEach(function(attrY, row){
+            cells.push(new SplomCell('movie_year', attrY, 0, row));
+    });
+
+    d3.csv(dataDir + 'movies.csv',
+        // Load data and use this function to process each row
+        function(row) {
+            return {
+                'index': +row['index'],
+                'movie_id': row['movie_id'],
+                'mm_percent': +row['mm_percent'],
+                'mf_percent': +row['mf_percent'],
+                'ff_percent': +row['ff_percent'],
+                'movie_title': row['movie_title'],
+                'movie_year': +row['movie_year'],
+                'imdb_rating': +row['imdb_rating'],
+                'imdb_votes': +row['imdb_votes'],
+                'genres': row['genres'],
+                'bechdel': row['bechdel'],
+                'sentiment': +row['sentiment'],
+                'tot_conv': +row['tot_conv'],
+                'affect_conv': +row['affect_conv'],
+                'posemo_conv': +row['posemo_conv'],
+                'negemo_conv': +row['negemo_conv'],
+                'anx_conv': +row['anx_conv'],
+                'anger_conv': +row['anger_conv'],
+                'sad_conv': +row['sad_conv'],
+                'sexual_conv': +row['sexual_conv'],
+                'work_conv': +row['work_conv'],
+                'leisure_conv': +row['leisure_conv'],
+                'home_conv': +row['home_conv'],
+                'money_conv': +row['money_conv'],
+                'religion_conv': +row['religion_conv'],
+                'death_conv': +row['death_conv'],
+                'swear_conv': +row['swear_conv'],
+                'netspeak_conv': +row['netspeak_conv'],
+
+            };
+        },
+        function(error, dataset) {
+            // Log and return from an error
+            if(error) {
+                console.error('Error while loading ./letter_freq.csv dataset.');
+                console.error(error);
+                return;
+            }
+            movies = dataset;
+            // Create map for each attribute's extent
+            dataAttributes.forEach(function(attribute){
+                extentByAttribute[attribute] = d3.extent(dataset, function(d){
+                    return d[attribute];
+                });
+            });
+
+            //Render Axes
+            xScale.domain(d3.extent(dataset, function(d){return d['movie_year'];}));
+            chartG.append('g')
+                .attr('class', 'x axis')
+                .attr('transform', function(d,i) {
+                    return 'translate('+[0, chartHeight*N]+')';
+                })
+                .call(xAxis)
+                .append('text')
+                .text('Movie Decades')
+                .attr('class', 'axis-label')
+                .attr('transform', 'translate('+[chartWidth/2 , 30]+')');
+
+            chartG.selectAll('.y.axis')
+                .data(dataAttributes)
+                .enter()
+                .append('g')
+                .attr('class', 'y axis')
+                .attr('transform', function(d,i) {
+                    return 'translate('+[0, i * chartHeight + chartpad / 2]+')';
+                })
+                .each(function(attribute){
+                    yScale.domain(extentByAttribute[attribute]);
+                    d3.select(this).call(yAxis);
+                    d3.select(this).append('text')
+                        .text(attribute)
+                        .attr('class', 'axis-label')
+                        .attr('transform', 'translate('+[-26, chartHeight / 2]+')rotate(270)');
+                });
+
+            cellEnter = chartG.selectAll('.cell')
+            .data(cells)
+            .enter()
+            .append('g')
+            .attr('class', 'cell')
+            .attr("transform", function(d) {
+                // Start from the far right for columns to get a better looking chart
+                var tx = 0;
+                var ty = d.row * chartHeight + chartpad / 2;
+                return "translate("+[tx, ty]+")";
+             }); 
+
+             /*cellEnter.each(function(cell){
+                cell.init(this);
+                cell.update(this, dataset);
+            });*/
+            updateChart(movies);
+    });
+
+    function updateChart(movies) { 
+        cellEnter.each(function(cell){
+         cell.init(this);
+         cell.update(this, movies);
+        });
+    }
+
+    //Splom Cell Methods
+    //Initialize
+    SplomCell.prototype.init = function(g) {
+        var cell = d3.select(g);
+
+        cell.append('rect')
+          .attr('class', 'frame')
+          .attr('width', chartWidth - chartpad)
+          .attr('height', chartHeight - chartpad);
+    }
+
+    //Update function
+    SplomCell.prototype.update = function(g, data) {
+        var cell = d3.select(g);
+
+        // Update the global x,yScale objects for this cell's x,y attribute domains
+        xScale.domain(d3.extent(movies, function(d){return d['movie_year'];}));
+        yScale.domain(extentByAttribute[this.y]);
+
+        // Save a reference of this SplomCell, to use within anon function scopes
+        var _this = this;
+
+        var dots = cell.selectAll('.dot')
+            .data(data, function(d){
+                return d.movie_id; // Create a unique id for movie
+            });
+
+        var dotsEnter = dots.enter()
+            .append('circle')
+            .attr('class', 'dot')
+            .style("fill", function(d) { return colorScale(d.bechdel); })
+            .attr('r', 4)
+            .on('click', function(d,i){
+                var movieid = d.movie_id;
+                var selected = d3.select(this);
+                if(selected.classed('selected')){
+                    //selected.classed('selected', false);
+                    svg.selectAll(".dot")
+                    .classed("selected", function(d){
+                        if(d.movie_id == movieid){
+                            return false;
+                        };
+                    })
+                    svg.selectAll(".dot")
+                    .classed("hidden", false);
+                }
+                else{
+                    //selected.classed('selected', true);
+                    svg.selectAll(".dot")
+                    .classed("selected", function(d){
+                        return d.movie_id == movieid;
+                    })
+                    svg.selectAll(".dot")
+                        .classed("hidden", function(d){
+                            return d.movie_id != movieid;
+                        })
+                }
+            });
+
+
+        dots.merge(dotsEnter)
+        .transition()
+        .duration(550)
+        .attr('cx', function(d){
+                return xScale(d[_this.x]);
+            })
+            .attr('cy', function(d){
+                return yScale(d[_this.y]);
+            });
+
+        dots.exit().remove();
+    }
+    
+    onGenreChanged = function() {
+        var select = d3.select('#genreAttrSelector').node();
+        // Get current value of select element
+        genre = select.options[select.selectedIndex].value;
+        //console.log(genre);  
+        var genreMovies = movies.filter(function(d){
+            console.log(d);
+            return d.genres.indexOf(genre) >= 0;
+        });
+        //console.log(genreMovies);
+        updateChart(genreMovies);
+        }
+})();
+    //Genre Change Function
+
